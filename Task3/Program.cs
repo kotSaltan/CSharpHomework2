b25d8f2378a384bfe67dd1f251739d969191f1bf
Anna Dorosheva <dorosheva.av@gmail.com>
2023-09-28 14:21:32 +0300

    task3

---
+++ Task3/Program.cs
@@ -0,0 +1,39 @@
+﻿// программа принимает цифру (день недели) и проверяет на выходной
+
+int Prompt (string message)
+{
+    Console.Write(message);
+    string value = Console.ReadLine();
+    int result = Convert.ToInt32(value);
+    return result;
+}
+
+bool IsWeekend(int weekDay)
+{
+    if (weekDay > 5)
+    {
+        return true;
+    }
+    return false;
+}
+bool ValidateWeekday (int number)
+{
+    if (number > 0 && number <= 7)
+    {
+        return true;
+    }
+    Console.WriteLine("Wring number format");
+    return false;
+}
+int weekDay = Prompt ("Enter weekday number: ");
+if (ValidateWeekday(weekDay))
+{
+    if (IsWeekend(weekDay))
+    {
+        Console.WriteLine("Weekend");
+    }
+    else
+    {
+        Console.WriteLine("Weekday");
+    }
+}
